715a0dd1543464502dafae05331afc7bcd1fc68f
Yzee <yan.zhigurs@mail.ru>
2022-12-21 22:05:09 +0400

    Adding input fields

--- Authorization screen/ViewController.swift
+++ Authorization screen/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var addBackground: UIImageView!
     @IBOutlet weak var loginLabel: UILabel!
-    
+    @IBOutlet weak var textFieldUserName: UITextField!
+    @IBOutlet weak var textFieldUserPassword: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,8 +20,21 @@ class ViewController: UIViewController {
         loginLabel.text = "Login"
         loginLabel.font = UIFont(name: "ChalkboardSE-Bold", size: 32)
         loginLabel.textColor = .white
+        
+        textFieldUserName.borderStyle = .none
+        textFieldUserName.clipsToBounds = true
+        textFieldUserName.layer.cornerRadius = 20
+        textFieldUserName.backgroundColor = .white
+        textFieldUserName.placeholder = "Login"
+        
+        textFieldUserPassword.borderStyle = .none
+        textFieldUserPassword.clipsToBounds = true
+        textFieldUserPassword.layer.cornerRadius = 20
+        textFieldUserPassword.backgroundColor = .white
+        textFieldUserPassword.placeholder = "Password"
     }
 
 
 }
+
 
