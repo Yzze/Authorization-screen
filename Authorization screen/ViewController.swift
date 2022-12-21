24bcfe77f1162952efa08ed90d05b5f1ae799217
Yzee <yan.zhigurs@mail.ru>
2022-12-21 21:24:34 +0400

    Add login label

--- Authorization screen/ViewController.swift
+++ Authorization screen/ViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var addBackground: UIImageView!
+    @IBOutlet weak var loginLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loginLabel.text = "Login"
+        loginLabel.font = UIFont(name: "ChalkboardSE-Bold", size: 32)
+        loginLabel.textColor = .white
     }
 
 
