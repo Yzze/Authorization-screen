e3f87331d71f0fc81ed542f1d79668b0c3488a26
Yzee <yan.zhigurs@mail.ru>
2022-12-21 23:23:36 +0400

    adding an extension and icons in textField

--- Authorization screen/ViewController.swift
+++ Authorization screen/ViewController.swift
@@ -26,15 +26,36 @@ class ViewController: UIViewController {
         textFieldUserName.layer.cornerRadius = 20
         textFieldUserName.backgroundColor = .white
         textFieldUserName.placeholder = "Login"
+        textFieldUserName.setIcon("iconsUser", .left)
         
         textFieldUserPassword.borderStyle = .none
         textFieldUserPassword.clipsToBounds = true
         textFieldUserPassword.layer.cornerRadius = 20
         textFieldUserPassword.backgroundColor = .white
         textFieldUserPassword.placeholder = "Password"
+        textFieldUserPassword.setIcon("iconsLock", .left)
     }
 
 
 }
 
-
+extension UITextField {
+    enum Direction {
+        case left, right
+    }
+    
+    func setIcon(_ image: String, _ direction: Direction) {
+        let iconView = UIImageView(frame: CGRect(x: 20, y: 5, width: 20, height: 20))
+        iconView.image = UIImage(named: image)
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 50, height: 30))
+        iconContainerView.addSubview(iconView)
+        if Direction.left == direction {
+            leftView = iconContainerView
+            leftViewMode = .always
+        } else {
+            self.rightView = iconContainerView
+            self.rightViewMode = .always
+        }
+        
+    }
+}
