38a1a23300ca5bf10f3fb723ea8c50534ae079f9
Yzee <yan.zhigurs@mail.ru>
2022-12-21 20:30:02 +0400

    Initial Commit

---
+++ Authorization screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Authorization screen
+//
+//  Created by Ян Жигурс on 21.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
