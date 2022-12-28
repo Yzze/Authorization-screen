041c9fc6745f70efaafb328e3ad33516999c1c8d
Yzee <yan.zhigurs@mail.ru>
2022-12-28 18:52:48 +0400

    changing the structure, adding an extension file for TextField

--- Authorization screen/Sources/ViewController.swift
+++ Authorization screen/Sources/ViewController.swift
@@ -93,27 +93,3 @@ class ViewController: UIViewController {
     }
 
 }
-
-
-
-extension UITextField {
-    enum Direction {
-        case left, right
-    }
-    
-    func setIcon(_ image: String, _ direction: Direction) {
-        let iconView = UIImageView(frame: CGRect(x: 20, y: 5, width: 20, height: 20))
-        iconView.image = UIImage(named: image)
-        let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 50, height: 30))
-        iconContainerView.addSubview(iconView)
-        if Direction.left == direction {
-            leftView = iconContainerView
-            leftViewMode = .always
-        } else {
-            self.rightView = iconContainerView
-            self.rightViewMode = .always
-        }
-        
-    }
-}
-
